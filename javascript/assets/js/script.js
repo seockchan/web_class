1d8a6a36ac07f420d753ea441918e6b040ffe34f
seockchan <sna12345@naver.com>
2022-01-19 10:55:52 +0900

    22.01.19_1

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,8 +1,6 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        document.querySelectorAll(".result > .result").forEach(function(ele){
-            el.display;
-        });
+        el.classList.toggle("active");
     });
 });
