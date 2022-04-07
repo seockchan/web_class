a36608cf6cfbe9754f40061a76b7bf2a105c3c44
1@1 <sna12345@naver.com>
2022-04-07 19:14:32 +0900

    22.04.07_1

---
+++ javascript/effect/musicPlayer/api/music.js
@@ -0,0 +1,68 @@
+let allMusic = [
+    {
+        name : "music1",
+        artist : "jone",
+        img : "music-01",
+        audio : "music-01"
+    },
+    {
+        name : "music2",
+        artist : "jone",
+        img : "music-02",
+        audio : "music-02"
+    },
+    {
+        name : "music3",
+        artist : "jone",
+        img : "music-03",
+        audio : "music-03"
+    },
+    {
+        name : "music4",
+        artist : "jone",
+        img : "music-04",
+        audio : "music-04"
+    },
+    {
+        name : "music4",
+        artist : "jone",
+        img : "music-04",
+        audio : "music-04"
+    },
+    {
+        name : "music5",
+        artist : "jone",
+        img : "music-05",
+        audio : "music-05"
+    },
+    {
+        name : "music6",
+        artist : "jone",
+        img : "music-06",
+        audio : "music-06"
+    },
+    {
+        name : "music7",
+        artist : "jone",
+        img : "music-07",
+        audio : "music-07"
+    },
+    {
+        name : "music8",
+        artist : "jone",
+        img : "music-08",
+        audio : "music-08"
+    },
+    {
+        name : "music9",
+        artist : "jone",
+        img : "music-09",
+        audio : "music-09"
+    },
+    {
+        name : "에일리 - 첫눈처럼 너에게 가겠다.",
+        artist : "에일리",
+        img : "music-10",
+        audio : "music-10"
+    }
+]
