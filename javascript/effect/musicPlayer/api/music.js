ab9e72f336fcacb1f70bc0eac5b43f56ce0ffb30
seockchan <sna12345@naver.com>
2022-04-08 14:19:41 +0900

    22.04.08_1

--- javascript/effect/musicPlayer/api/music.js
+++ javascript/effect/musicPlayer/api/music.js
@@ -1,68 +1,118 @@
 let allMusic = [
     {
         name : "music1",
-        artist : "jone",
+        artist : "1번가수",
         img : "music-01",
         audio : "music-01"
     },
     {
         name : "music2",
-        artist : "jone",
+        artist : "2번가수",
         img : "music-02",
         audio : "music-02"
     },
     {
         name : "music3",
-        artist : "jone",
+        artist : "3번가수",
         img : "music-03",
         audio : "music-03"
     },
     {
         name : "music4",
-        artist : "jone",
-        img : "music-04",
-        audio : "music-04"
-    },
-    {
-        name : "music4",
-        artist : "jone",
+        artist : "4번가수",
         img : "music-04",
         audio : "music-04"
     },
     {
         name : "music5",
-        artist : "jone",
+        artist : "5번가수",
         img : "music-05",
         audio : "music-05"
     },
     {
         name : "music6",
-        artist : "jone",
+        artist : "6번가수",
         img : "music-06",
         audio : "music-06"
     },
     {
         name : "music7",
-        artist : "jone",
+        artist : "7번가수",
         img : "music-07",
         audio : "music-07"
     },
     {
         name : "music8",
-        artist : "jone",
+        artist : "8번가수",
         img : "music-08",
         audio : "music-08"
     },
     {
         name : "music9",
-        artist : "jone",
+        artist : "9번가수",
         img : "music-09",
         audio : "music-09"
     },
     {
-        name : "에일리 - 첫눈처럼 너에게 가겠다.",
-        artist : "에일리",
+        name : "music10",
+        artist : "10번가수",
         img : "music-10",
         audio : "music-10"
-    }
+    },
+    {
+        name : "music11",
+        artist : "11번가수",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "music12",
+        artist : "12번가수",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "music13",
+        artist : "13번가수",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "music14",
+        artist : "14번가수",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "music15",
+        artist : "15번가수",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "music16",
+        artist : "16번가수",
+        img : "music-16",
+        audio : "music-16"
+    }, {
+        name : "music17",
+        artist : "17번가수",
+        img : "music-17",
+        audio : "music-17"
+    }, {
+        name : "music18",
+        artist : "18번가수",
+        img : "music-18",
+        audio : "music-18"
+    }, {
+        name : "music19",
+        artist : "19번가수",
+        img : "music-19",
+        audio : "music-19"
+    }, {
+        name : "music20",
+        artist : "20번가수",
+        img : "music-20",
+        audio : "music-20"
+    },
 ]
